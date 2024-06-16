072db1b561d56724db4af55a8320c4489cb1f08b
SHA.MISTULA <mistula.shaunpaulalexis@ue.edu.ph>
2024-06-16 22:23:26 +0800

    init commit

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const welcomeTextElement = document.querySelector('.welcomeText');
+    const texts = ['#SoarHighCpE', '#ChooseBSCpE', '#AIWarriors', 'Join Our Events!', 'Be a Part of SCpES'];
+    let currentIndex = 0;
+
+    function updateText() {
+      // Remove the animation class
+      welcomeTextElement.style.animation = 'none';
+      
+      // Trigger reflow to restart the animation
+      welcomeTextElement.offsetWidth; // reflow
+
+      // Update the text
+      welcomeTextElement.textContent = texts[currentIndex];
+      currentIndex = (currentIndex + 1) % texts.length;
+
+      // Re-apply the animation class
+      welcomeTextElement.style.animation = 
+        'typing 2s, cursor .4s step-end infinite alternate';
+    }
+
+    // Initial text update
+    updateText();
+
+    // Update text every 3 seconds
+    setInterval(updateText, 3000);
+  });
